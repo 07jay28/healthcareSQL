b8fd292c1587f857448d9c1c86113fa70f03a888
07jay28 <collegektejas@gmail.com>
2025-02-05 00:40:32 -0600

    started new function to create pie chart on searchtrend

--- KoreaCovid.py
+++ KoreaCovid.py
@@ -123,16 +123,32 @@ def searchTrend(cursor):
     # can also have a functionality for looking at a specific month only
     return 0
 
+# function to go through search trend and look at data changes for each symptom and plot on a pie char
+def searchTrend(cursor):
+    query = """ SELECT *
+                FROM SearchTrend"""
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+    for row in data:
+        date = row[0]
+        cold = row[1]
+        flu = row[2]
+        pneum = row[3]
+        covid = row[4]
+
+    # plot a pie chart interactive for each date
+    # can also have a functionality for looking at a specific month only
+    return 0
+
 
 # database connections
-CasesConn = sqlite3.connect('./KoreaCovidData/Case.db')
-PatientInfoConn = sqlite3.connect('./KoreaCovidData/PatientInfo.db')
-SearhConn = sqlite3.connect('./KoreaCovidData/SearhTrend.db')
+CaseConn = sqlite3.connect('./KoreaCovidData/Case.db')
+PatientConn = sqlite3.connect('./KoreaCovidData/PatientInfo.db')
 
 # database cursors
-CaseCursor = CasesConn.cursor()
-PatientCursor = PatientInfoConn.cursor()
-SearhCursor = SearhConn.cursor()
+CaseCursor = CaseConn.cursor()
+PatientCursor = PatientConn.cursor()
 
 # functions
 CasesDataPerProvince(CaseCursor)
